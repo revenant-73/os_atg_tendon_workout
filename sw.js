eb944cfbfcc0d692a599cdb0ee83ccade85f308c
Loren Anderson <154761885+revenant-73@users.noreply.github.com>
2026-01-01 11:51:37 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE = "daily-reset-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./sw.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
